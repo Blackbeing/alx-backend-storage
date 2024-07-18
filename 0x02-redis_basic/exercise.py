04f1420bc08c4881c9d80426e3e2dd2e044039d7
Blackbeing <jonnieey55@gmail.com>
2024-07-18 03:25:35 +0300

    feat Convert types

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,8 +4,9 @@ This module provides a class to interface with
 redis server via python
 """
 import uuid
+from typing import Callable, Optional, Union
+
 import redis
-from typing import Union
 
 
 class Cache:
@@ -24,3 +25,17 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str, int, bytes, float]:
+        raw_value = self._redis.get(key)
+        if fn is None:
+            return raw_value
+        return fn(raw_value)
+
+    def get_str(self, key: str) -> Union[str, int, bytes, float]:
+        return self.get(key, lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[str, int, bytes, float]:
+        return self.get(key, int)
